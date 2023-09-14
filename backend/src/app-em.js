b06693909b35e2a40f21064824ecf1cededef18d
Igor Machado <igor.gabriel.machado@gmail.com>
2023-09-14 00:35:26 -0300

    WebSocketsBackEnd

--- backend/src/app-em.js
+++ backend/src/app-em.js
@@ -1,6 +1,7 @@
+const WebSocket = require('ws');
 const crypto = require('./utils/crypto');
 
-module.exports = (settings) => {
+module.exports = (settings, wss) => {
 
     if (!settings) throw new Error(`Não é possível iniciar o bot sem configurações.`);
 
@@ -9,5 +10,13 @@ module.exports = (settings) => {
 
     exchange.miniTickerStream((markets) => {
         //console.log(markets);
+        if (!wss || !wss.clients) return;
+        wss.clients.forEach(client => {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send(JSON.stringify({ miniTicker: markets })); 
+            }
+        });
     })
+
+    console.log(`Bot consumidor de exchanges está rodando.`);
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,18 @@
 const database = require('./db');
 const app = require ('./app');
+const appWs = require('./app-ws');
 const settingsRepository = require('./repositories/settingsRepository');
 const appEm = require('./app-em');
 
 settingsRepository.getDefaultSettings()
     .then(settings => {
-        appEm(settings);
-
-        app.listen(process.env.PORT, () => {
+        const server = app.listen(process.env.PORT, () => {
             console.log('App está rodando em ' + process.env.PORT);
         })
+
+        const wss = appWs(server);
+
+        appEm(settings, wss);
     })
     .catch(err =>{
         console.error(err);
